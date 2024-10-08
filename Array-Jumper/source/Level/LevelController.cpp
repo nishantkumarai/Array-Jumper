336329e0b262c13628e3490aa53e01e3f5828d29
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-10-09 03:15:06 +0530

    Implement Player Movement

--- Array-Jumper/source/Level/LevelController.cpp
+++ Array-Jumper/source/Level/LevelController.cpp
@@ -1,43 +1,173 @@
-#include "../../header/Level/LevelController.h"
-#include "../../header/Level/LevelView.h"
+#include "../../header/Player/PlayerController.h"
+#include "../../header/Player/PlayerView.h"
+#include "../../header/Player/PlayerModel.h"
+#include "../../header/Global/ServiceLocator.h"
 #include "../../header/Level/LevelModel.h"
-#include "../../header/Level/LevelData.h"
-#include "../../header/Main/GameService.h"
+#include "../../header/Sound/SoundService.h"
 
-namespace Level
+
+
+namespace Player
 {
+	using namespace Global;
+	using namespace Event;
+	using namespace Level;
+	using namespace Sound;
 
-	LevelController::LevelController()
+	PlayerController::PlayerController()
 	{
-		level_view = new LevelView(this);
-		level_model = new LevelModel();
+		player_model = new PlayerModel();
+		player_view = new PlayerView(this);
 	}
 
-	LevelController::~LevelController() {}
+	PlayerController::~PlayerController() { destroy(); }
 
-	void LevelController::initialize()
+	void PlayerController::initialize()
 	{
-		level_view->initialize();
+		player_model->initialize();
+		player_view->initialize();
+
+		event_service = ServiceLocator::getInstance()->getEventService();
+
+		resetPlayer();
 	}
 
-	void LevelController::update()
+	void PlayerController::update()
 	{
-		level_view->update();
+		player_view->update();
+		readInput();
 	}
 
-	void LevelController::render()
+	void PlayerController::render()
 	{
-		level_view->render();
+		player_view->render();
 	}
 
-	BoxDimensions LevelController::getBoxDimensions()
+	PlayerState PlayerController::getPlayerState()
 	{
-		return level_view->getBoxDimensions();
+		return player_model->getPlayerState();
 	}
 
-	BlockType LevelController::getCurrentBoxValue(int currentPosition)
+	void PlayerController::setPlayerState(PlayerState new_player_state)
 	{
-		return level_model->getCurrentBoxValue(currentPosition);
+		player_model->setPlayerState(new_player_state);
 	}
 
+	int PlayerController::getCurrentPosition()
+	{
+		return player_model->getCurrentPosition();
+	}
+
+	int PlayerController::getCurrentLives()
+	{
+		return player_model->getCurrentLives();
+	}
+
+	void PlayerController::takeDamage()
+	{
+		player_model->decreamentLife();
+		if (player_model->getCurrentLives() <= 0)
+			onDeath();
+		else
+			player_model->resetPosition();
+	}
+
+
+	void PlayerController::destroy()
+	{
+		delete(player_model);
+		delete(player_view);
+	}
+
+	void PlayerController::resetPlayer()
+	{
+		player_model->resetPlayer();
+	}
+
+	void PlayerController::readInput()
+	{
+		if (event_service->pressedRightArrowKey() || event_service->pressedDKey())
+		{
+			if (event_service->heldSpaceKey())
+				jump(MovementDirection::FORWARD);
+			else
+				move(MovementDirection::FORWARD);
+		}
+		if (event_service->pressedLeftArrowKey() || event_service->pressedAKey())
+		{
+			if (event_service->heldSpaceKey())
+				jump(MovementDirection::BACKWARD);
+			else
+				move(MovementDirection::BACKWARD);
+		}
+
+	}
+
+	void PlayerController::move(MovementDirection direction)
+	{
+		int steps, targetPosition;
+		switch (direction)
+		{
+		case MovementDirection::FORWARD:
+			steps = 1;
+			break;
+		case MovementDirection::BACKWARD:
+			steps = -1;
+			break;
+		default:
+			steps = 0;
+			break;
+		}
+
+		targetPosition = player_model->getCurrentPosition() + steps;
+
+		if (!isPositionInBound(targetPosition))
+			return;
+
+		player_model->setCurrentPosition(targetPosition);
+		ServiceLocator::getInstance()->getSoundService()->playSound(SoundType::MOVE);
+		ServiceLocator::getInstance()->getGameplayService()->onPositionChanged(targetPosition);
+	}
+
+	void PlayerController::jump(MovementDirection direction)
+	{
+		int current_position = player_model->getCurrentPosition();
+		BlockType box_value = ServiceLocator::getInstance()->getLevelService()->getCurrentBoxValue(current_position);
+		int steps, targetPosition;
+
+		switch (direction)
+		{
+		case MovementDirection::FORWARD:
+			steps = box_value;
+			break;
+		case MovementDirection::BACKWARD:
+			steps = -box_value;
+			break;
+		default:
+			steps = 0;
+			break;
+		}
+
+		targetPosition = current_position + steps;
+
+		if (!isPositionInBound(targetPosition))
+			return;
+
+		player_model->setCurrentPosition(targetPosition);
+		ServiceLocator::getInstance()->getSoundService()->playSound(SoundType::JUMP);
+		ServiceLocator::getInstance()->getGameplayService()->onPositionChanged(targetPosition);
+	}
+
+	bool PlayerController::isPositionInBound(int targetPosition)
+	{
+		if (targetPosition >= 0 && targetPosition < LevelData::NUMBER_OF_BOXES)
+			return true;
+		return false;
+	}
+
+	void PlayerController::onDeath()
+	{
+		ServiceLocator::getInstance()->getGameplayService()->onDeath();
+		player_model->resetPlayer();
+	}
 }
