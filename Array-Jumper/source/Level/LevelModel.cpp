c8be8943828922a014393a8a040f4e4f53b612ef
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-10-09 00:15:36 +0530

    Create Data for Level

--- Array-Jumper/source/Level/LevelModel.cpp
+++ Array-Jumper/source/Level/LevelModel.cpp
@@ -2,5 +2,11 @@
 
 namespace Level
 {
-	
+	LevelModel::LevelModel() {}
+	LevelModel::~LevelModel() {}
+
+	BlockType LevelModel::getCurrentBoxValue(int currentPosition)
+	{
+		return current_level_data.level_boxes[currentPosition];
+	}
 }
